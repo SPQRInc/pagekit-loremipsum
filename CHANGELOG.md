6ceb1740e6f70e4fb903a1bad451ac481bb79c0c
Roman <roman.lossin@icloud.com>
2017-05-30 11:54:37 +0200

    ADD: Added French language (Thanks to Jean-Francois Huet/f1evm.fr for translating this extension)

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 1.0.1
+
+- ADD: Added French language (Thanks to Jean-Francois Huet/f1evm.fr for translating this extension)
+
+# 1.0.0
+
+- Initial version
---
+++ languages/fr_FR/messages.php
@@ -0,0 +1,8 @@
+<?php return [
+	'Insert Text' => 'Insérer un Texte',
+	'Count'       => 'Nombre',
+	'Type'        => 'Type',
+	'Words'       => 'Mots',
+	'Sentences'   => 'Phrases',
+	'Paragraphs'  => 'Paragraphes'
+];
